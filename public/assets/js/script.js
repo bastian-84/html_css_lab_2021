a355700ce42fe7b03d54c3dbaee6234ba109616a
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-11-15 14:51:32 -0800

    SR: some stuff done

---
+++ public/assets/js/script.js
@@ -0,0 +1,18 @@
+// ~~~~~~~~~~~~ Hilfs-Funktion ~~~~~~~~~~~ //
+function getEl(para1) {
+    var output = document.getElementById(para1);
+    return output;
+};
+
+// ~~~~~~~~~~~~~~~~~~ JS ~~~~~~~~~~~~~~~~~ // 
+
+console.log('test');
+
+
+
+// ~~~~~~~~~~~~~~~~ jQuery ~~~~~~~~~~~~~~~ //
+$(document).ready(function () {
+
+    // jQuery methods go here...
+
+});
